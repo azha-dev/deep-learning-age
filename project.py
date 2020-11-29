94e5f6c3f9258a29ed1aff1e42cfc7f89c755d31
azha <anatole.zha@gmail.com>
2020-11-29 11:29:26 +0100

    Update

--- project.py
+++ project.py
@@ -19,17 +19,19 @@ class CNN(nn.Module):
         self.pooling1 = nn.MaxPool2d(2, 2)
         self.convolution2 = nn.Conv2d(6, FILTERS_2, KERNEL_2) #Nombre de filtres de la 1ère conv, nouveau nombre de filtres
         self.pooling2 = nn.MaxPool2d(2, 2) 
-        self.layer1  = nn.Linear(KERNEL_2 * KERNEL_2 * FILTERS_2, 100) #Le plus dur à calculer, c'est le 5x5x6 (prise de tête, regarder la doc avec la formule)
+        self.layer1  = nn.Linear(35344, 100) #Le plus dur à calculer, c'est le 5x5x6 (prise de tête, regarder la doc avec la formule)
         self.layer2  = nn.Linear(100, 50)
         self.layer3  = nn.Linear(50, 10)
+        self.layer4  = nn.Linear(10, 1)
         
     def forward(self, x):
         x = self.pooling1(torch.nn.functional.relu(self.convolution(x)))
         x = self.pooling2(torch.nn.functional.relu(self.convolution2(x)))
-        x = x.view(-1, FILTERS_2 * KERNEL_2 * KERNEL_2) #Convertit le tenseur en vecteur
+        x = x.view(-1, 35344) #Convertit le tenseur en vecteur
         x = torch.nn.functional.relu(self.layer1(x))
         x = torch.nn.functional.relu(self.layer2(x))
-        x = self.layer3(x)
+        x = torch.nn.functional.relu(self.layer3(x))
+        x = self.layer4(x)
         return x
     
 if __name__ == "__main__":
@@ -38,13 +40,9 @@ if __name__ == "__main__":
    
     loader = torch.utils.data.DataLoader(ages_dataset)
 
-    print(ages_dataset[0])
     print(len(ages_dataset))
     img, label = ages_dataset[0]
     print(img.size(), label)
-
-    print(type(img))
-    print(type(label))
 
     #npimg = img.numpy()
     #npimg = np.transpose(npimg, (1, 2, 0))
@@ -54,15 +52,16 @@ if __name__ == "__main__":
     cnn = CNN()
 
     optimizer = torch.optim.SGD(cnn.parameters(), lr=1e-4)
-    errorFunction = nn.CrossEntropyLoss()
+    errorFunction = nn.MSELoss()
 
 
     for iteration in range(1):
         for i, img in enumerate(loader):
             inputs, label = img
-
+            
             optimizer.zero_grad() #Initialisation de l'optimiseur
             output = cnn(inputs)
+            label = label.type(torch.FloatTensor)
             error = errorFunction(output, label)
             error.backward()
             optimizer.step()
